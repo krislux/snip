e3ed05f01aaea0bc7e7fec34991c4d2ca469b1c6
Kris Lux <kris.lux@eksponent.com>
2019-02-06 13:37:03 +0100

    Added nodemon support

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,124 @@
-!function(n){var o={};function r(e){if(o[e])return o[e].exports;var t=o[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,r),t.l=!0,t.exports}r.m=n,r.c=o,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(t,e){if(1&e&&(t=r(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)r.d(n,o,function(e){return t[e]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,n){"use strict";var o,r=n(1),a=(o=r)&&o.__esModule?o:{default:o};n(2),window.editors={};var s=["html","css","javascript"],i="//localhost:8222";if(s.forEach(function(e){var t=ace.edit("editor-"+e);t.setTheme("ace/theme/monokai"),t.session.setMode("ace/mode/"+e),editors[e]=t}),[].forEach.call(document.querySelectorAll(".btn-save"),function(e){e.addEventListener("click",function(){var t={};s.forEach(function(e){t[e]=editors[e].session.getValue()}),(0,a.default)({url:i+"/save",contentType:"application/json",method:"post",data:t}).then(function(e){e.responseJSON&&e.responseJSON.success?history.pushState(null,null,"#/"+e.responseJSON.id):alert("Something went wrong.")})})}),location.hash){var u=location.hash.match(/#\/(\w{7})/)[1];(0,a.default)({url:i+"/get/"+u,contentType:"application/json",method:"get"}).then(function(t){t.responseJSON&&(s.forEach(function(e){editors[e].session.setValue(t.responseJSON[e])}),document.getElementById("editor-preview").src=i+"/render/"+u)})}},function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=function(e,t){if(Array.isArray(e))return e;if(Symbol.iterator in Object(e))return function(e,t){var n=[],o=!0,r=!1,a=void 0;try{for(var s,i=e[Symbol.iterator]();!(o=(s=i.next()).done)&&(n.push(s.value),!t||n.length!==t);o=!0);}catch(e){r=!0,a=e}finally{try{!o&&i.return&&i.return()}finally{if(r)throw a}}return n}(e,t);throw new TypeError("Invalid attempt to destructure non-iterable instance")},a="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e};t.default=
-/**
- * Super simple AJAX Promise helper.
- * 
- * @author  Kris Lux <mail@kilolima.dk>
- * @version 1.0
- * @license MIT
- */
-function(o){"string"==typeof o&&(o={url:o});return new Promise(function(e,t){var n=new XMLHttpRequest;n.addEventListener("readystatechange",function(){4===this.readyState&&(200<=this.status&&this.status<=299?(/^application\/json/.test(this.getResponseHeader("Content-Type"))&&(this.responseJSON=JSON.parse(this.response)),e(this)):t(Error(this.statusText)))}),n.open((o.method||"GET").toUpperCase(),o.url,o.async||!0,o.user||null,o.password||null),void 0===o.headers&&(o.headers={"Content-Type":"application/x-www-form-urlencoded"}),void 0!==o.contentType&&(o.headers["Content-Type"]=o.contentType),Object.keys(o.headers||{}).forEach(function(e){o.headers[e]&&n.setRequestHeader(e,o.headers[e])}),"object"==a(o.data)&&/^application\/json/.test(o.headers["Content-Type"])?o.data=JSON.stringify(o.data):"object"!=a(o.data)||o.data instanceof FormData||(o.data=Object.entries(o.data).map(function(e){var t=r(e,2),n=t[0],o=t[1];return encodeURIComponent(n)+"="+encodeURIComponent(o)}).join("&")),n.send(o.data||null)})}},function(e,t,n){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./client/js/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./client/js/app.js":
+/*!**************************!*\
+  !*** ./client/js/app.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n/* eslint-env browser */\n/* global ace, editors */\n\nvar _ajax = __webpack_require__(/*! ./lib/ajax.js */ \"./client/js/lib/ajax.js\");\n\nvar _ajax2 = _interopRequireDefault(_ajax);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n__webpack_require__(/*! ../sass/main.scss */ \"./client/sass/main.scss\");\n\nwindow.editors = {};\n\nvar types = ['html', 'css', 'javascript'];\n\nvar backend = '//localhost:8222';\n\n/**\n * Initialize all editors\n */\ntypes.forEach(function (type) {\n    var editor = ace.edit('editor-' + type);\n    editor.setTheme('ace/theme/monokai');\n    editor.session.setMode('ace/mode/' + type);\n    editors[type] = editor;\n});\n\n/**\n * Saving\n */\n[].forEach.call(document.querySelectorAll('.btn-save'), function ($el) {\n    $el.addEventListener('click', function () {\n\n        var data = {};\n        types.forEach(function (i) {\n            data[i] = editors[i].session.getValue();\n        });\n\n        (0, _ajax2.default)({\n            url: backend + '/save',\n            contentType: 'application/json',\n            method: 'post',\n            data: data\n        }).then(function (res) {\n            if (res.responseJSON && res.responseJSON.success) {\n                history.pushState(null, null, '#/' + res.responseJSON.id);\n            } else {\n                alert('Something went wrong.');\n            }\n        });\n    });\n});\n\n/**\n * Read existing data if location.hash set.\n */\nif (location.hash) {\n    var id = location.hash.match(/#\\/(\\w{7})/)[1];\n    (0, _ajax2.default)({\n        url: backend + '/get/' + id,\n        contentType: 'application/json',\n        method: 'get'\n    }).then(function (res) {\n        if (res.responseJSON) {\n            types.forEach(function (i) {\n                editors[i].session.setValue(res.responseJSON[i]);\n            });\n            document.getElementById('editor-preview').src = backend + '/render/' + id;\n        }\n    });\n}\n\n//# sourceURL=webpack:///./client/js/app.js?");
+
+/***/ }),
+
+/***/ "./client/js/lib/ajax.js":
+/*!*******************************!*\
+  !*** ./client/js/lib/ajax.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\n\nvar _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i[\"return\"]) _i[\"return\"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError(\"Invalid attempt to destructure non-iterable instance\"); } }; }();\n\nvar _typeof = typeof Symbol === \"function\" && typeof Symbol.iterator === \"symbol\" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === \"function\" && obj.constructor === Symbol && obj !== Symbol.prototype ? \"symbol\" : typeof obj; };\n\nexports.default = ajax;\n/**\n * Super simple AJAX Promise helper.\n * \n * @author  Kris Lux <mail@kilolima.dk>\n * @version 1.0\n * @license MIT\n */ /**\n    * Does not support Internet Explorer at all, as it doesn't support Promises.\n    * This can be fixed with a Promise polyfill.\n    * Support: Edge 12+, Firefox 29+, Chrome 33+, Safari 7.1+, Android 4.4.4+, iOS Safari 8+.\n    * \n    * Usage:\n    *      ajax('file.json').then(xhr => { ... });\n    * or\n    *      ajax({ url: 'file.json', method: 'post' }).then(xhr => { ... });\n    * \n    * @param   {(object|string)}   options               // If string this is the url, if object see below.\n    * @param   {string}   [options.method]               // HTTP method to use, defaults to GET.\n    * @param   {string}   [options.url]                  // Any valid url to call.\n    * @param   {(object|FormData|string)} [options.data] // Body data for POST etc.\n    * @param   {object}   [options.headers]              // Custom headers in {name:value} format.\n    * @param   {string}   [options.contentType]          // Shorthand for Content-Type header.\n    * @param   {boolean}  [options.async]                // Defaults to true, pass false to block execution.\n    * @param   {string}   [options.user]                 // Authentication username.\n    * @param   {string}   [options.password]             // Authentication password.\n    * \n    * @return  {Promise}  Arg is XmlHttpRequest with added properties responseJSON (if JSON).\n    */\nfunction ajax(options) {\n\n    if (typeof options === 'string') options = { url: options };\n\n    return new Promise(function (resolve, reject) {\n\n        var xhr = new XMLHttpRequest();\n\n        xhr.addEventListener('readystatechange', function () {\n            if (this.readyState === 4) {\n                if (this.status >= 200 && this.status <= 299) {\n                    if (/^application\\/json/.test(this.getResponseHeader('Content-Type'))) {\n                        this.responseJSON = JSON.parse(this.response);\n                    }\n                    resolve(this);\n                } else {\n                    reject(Error(this.statusText));\n                }\n            }\n        });\n\n        xhr.open((options.method || 'GET').toUpperCase(), options.url, options.async || true, options.user || null, options.password || null);\n\n        /**\n         * Set headers, including shorthand for setting the Content-Type header.\n         */\n        if (typeof options.headers === 'undefined') options.headers = {\n            'Content-Type': 'application/x-www-form-urlencoded'\n        };\n        if (typeof options.contentType !== 'undefined') {\n            options.headers['Content-Type'] = options.contentType;\n        }\n        Object.keys(options.headers || {}).forEach(function (key) {\n            if (options.headers[key]) {\n                xhr.setRequestHeader(key, options.headers[key]);\n            }\n        });\n\n        /**\n         * If Content-Type is set to JSON, stringify data objects.\n         */\n        if (_typeof(options.data) == 'object' && /^application\\/json/.test(options.headers['Content-Type'])) {\n            options.data = JSON.stringify(options.data);\n        }\n\n        /**\n         * Convert data objects (except FormData) to &url=param strings.\n         * Currently this only works for single dimension objects.\n         */\n        else if (_typeof(options.data) == 'object' && !(options.data instanceof FormData)) {\n                options.data = Object.entries(options.data).map(function (_ref) {\n                    var _ref2 = _slicedToArray(_ref, 2),\n                        key = _ref2[0],\n                        value = _ref2[1];\n\n                    return encodeURIComponent(key) + '=' + encodeURIComponent(value);\n                }).join('&');\n            }\n        xhr.send(options.data || null);\n    });\n}\n\n//# sourceURL=webpack:///./client/js/lib/ajax.js?");
+
+/***/ }),
+
+/***/ "./client/sass/main.scss":
+/*!*******************************!*\
+  !*** ./client/sass/main.scss ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./client/sass/main.scss?");
+
+/***/ })
+
+/******/ });
